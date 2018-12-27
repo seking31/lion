e5473c4b7d429a048645903bc6bc54aa1557641f
SaraKing <sking@vertafore.com>
2018-12-27 16:26:57 -0700

    Removed id from models

--- lionApp/src/models/reviewModel.js
+++ lionApp/src/models/reviewModel.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const ReviewSchema = new Schema({
-    id: {
-        type: Number
-    },
     title: {
         type: String
     },
